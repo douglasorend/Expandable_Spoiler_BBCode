67dd0aaa04e746298b897e250c054d6bd5bcb1bf
douglasorend <37510276+douglasorend@users.noreply.github.com>
2018-10-18 18:59:30 -0500

    v1.5 - December 30th, 2014
    v1.5 - December 30th, 2014
    o language.xml file renamed to english.xml
    o Added new form of the bbcode: [spoiler=Some Text]Content Here[/spoiler]
    o Changed bbcode definition code based on bbcodes defined in Sources/Subs.php...

--- Subs-BBCode-Spoiler.php
+++ Subs-BBCode-Spoiler.php
@@ -1,6 +1,6 @@
 <?php
 /**********************************************************************************
-* Subs-BBCode-spoiler.php
+* Subs-BBCode-Spoiler.php
 ***********************************************************************************
 ***********************************************************************************
 * This program is distributed in the hope that it is and will be useful, but      *
@@ -10,19 +10,18 @@
 
 function BBCode_Spoiler(&$bbc)
 {
+	global $txt;
+
 	$bbc[] = array(
 		'tag' => 'spoiler',
-		'type' => 'unparsed_content',
-		'dcontent' => '<div style="padding: 3px; font-size: 1em;"><div style="text-transform: uppercase; border-bottom: 1px solid #5873B0; margin-bottom: 3px; font-size: 0.8em; font-weight: bold; display: block;"><span onClick="if (this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display != \'\') {  this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display = \'\'; this.innerHTML = \'<b>xu7aprafafrA: </b><a href=\\\'#\\\' onClick=\\\'return false;\\\'>xu7aprafafrC</a>\'; } else { this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display = \'none\'; this.innerHTML = \'<b>xu7aprafafrA: </b><a href=\\\'#\\\' onClick=\\\'return false;\\\'>xu7aprafafrB</a>\'; }" /><b>xu7aprafafrA: </b><a href="#" onClick="return false;">xu7aprafafrB</a></span></div><div class="quotecontent"><div style="display: none;">$1</div></div></div>',
-		'validate' => isset($disabled['spoiler']) ? null : create_function('&$tag, &$data, $disabled', '
-			global $txt;
-			if (!isset($tag["content"]))
-			{
-				$tmp = str_replace("xu7aprafafrA", $txt["spoiler"], $tag["dcontent"]);
-				$tmp = str_replace("xu7aprafafrC", $txt["debug_hide"], str_replace("xu7aprafafrB", $txt["debug_show"], $tmp));
-				$tag["content"] = $tmp;
-			}
-		'),
+		'before' => '<div style="padding: 3px; font-size: 1em;"><div style="text-transform: uppercase; border-bottom: 1px solid #5873B0; margin-bottom: 3px; font-size: 0.8em; font-weight: bold; display: block;"><span onClick="if (this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display != \'\') {  this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display = \'\'; this.innerHTML = \'<b>' . $txt['spoiler'] . ': </b><a href=\\\'#\\\' onClick=\\\'return false;\\\'>' . $txt["debug_hide"] . '</a>\'; } else { this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display = \'none\'; this.innerHTML = \'<b>' . $txt['spoiler'] . ': </b><a href=\\\'#\\\' onClick=\\\'return false;\\\'>' . $txt["debug_show"] . '</a>\'; }" /><b>' . $txt['spoiler'] . ': </b><a href="#" onClick="return false;">' . $txt["debug_show"] . '</a></span></div><div class="quotecontent"><div style="display: none;">',
+		'after' => '</div></div></div>',
+	);
+	$bbc[] = array(
+		'tag' => 'spoiler',
+		'type' => 'parsed_equals',
+		'before' => '<div style="padding: 3px; font-size: 1em;"><div style="text-transform: uppercase; border-bottom: 1px solid #5873B0; margin-bottom: 3px; font-size: 0.8em; font-weight: bold; display: block;"><span onClick="if (this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display != \'\') {  this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display = \'\'; this.innerHTML = \'<b>' . $txt['spoiler'] . ': $1 &#149; </b><a href=\\\'#\\\' onClick=\\\'return false;\\\'>' . $txt["debug_hide"] . '</a>\'; } else { this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display = \'none\'; this.innerHTML = \'<b>' . $txt['spoiler'] . ': $1 &#149; </b><a href=\\\'#\\\' onClick=\\\'return false;\\\'>' . $txt["debug_show"] . '</a>\'; }" /><b>' . $txt['spoiler'] . ': $1 &#149; </b><a href="#" onClick="return false;">' . $txt["debug_show"] . '</a></span></div><div class="quotecontent"><div style="display: none;">',
+		'after' => '</div></div></div>',
 	);
 }
 
--- readme.txt
+++ readme.txt
@@ -1,5 +1,5 @@
 [hr]
-[center][color=red][size=16pt][b]EXPANDING & COLLAPSING SPOILER TAG v1.4[/b][/size][/color]
+[center][color=red][size=16pt][b]EXPANDING & COLLAPSING SPOILER TAG v1.5[/b][/size][/color]
 [url=http://www.simplemachines.org/community/index.php?action=profile;u=253913][b]By Dougiefresh[/b][/url] -> [url=http://custom.simplemachines.org/mods/index.php?mod=3981]Link to Mod[/url]
 [/center]
 [hr]
@@ -8,16 +8,22 @@
 This modification adds a BBCode to the forum that allows the post author to hide a spoiler in the post, which is clickable to display it to the user.
 
 This BBCode takes the form:
-[code][spoiler]{content goes here}[/spoiler][/code]
+[code][nobbc][spoiler=Some Text]{content goes here}[/spoiler]
+[spoiler]{content goes here}[/spoiler][/nobbc][/code]
 
 [color=blue][b][size=12pt][u]Admin Settings[/u][/size][/b][/color]
-There are no admin settings to this mod.  To disable, you must uninstall this mod.
+This BBCode may be disabled by going into the [b]Admin[/b] => [b]Forum[/b] => [b]Posts and Topics[/b] => [b]Bulletin Board Code[/b] and unchecking the bbcodes you don't want to use.  You may also be uninstall this mod in order to disable it.
 
 [color=blue][b][size=12pt][u]Compatibility Notes[/u][/size][/b][/color]
 This mod was tested on SMF 2.0.9, but should work on SMF 2.0 and up.
 
-[color=blue][b][size=12pt][u]Changelog[/u][/size][/b][/color]
+[color=blue][b][size=12pt][u]spoiler[/u][/size][/b][/color]
 [quote]
+[b][u]v1.5 - December 30th, 2014[/u][/b]
+o [b]language.xml[/b] file renamed to [b]english.xml[/b]
+o Added new form of the bbcode: [b][nobbc][spoiler=Some Text]Content Here[/spoiler][/nobbc][/b]
+o Changed bbcode definition code based on bbcodes defined in [b]Sources/Subs.php[/b]...
+
 [b][u]v1.4 - December 29th, 2014[/u][/b]
 o Fixed image install locations within [b]package-info.xml[/b].
 
