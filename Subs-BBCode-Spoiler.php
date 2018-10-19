aa72595f40133c6dee4f706b089149a68f16a1fd
douglasorend <37510276+douglasorend@users.noreply.github.com>
2018-10-18 19:01:56 -0500

    v1.10 - July 20th, 2015
    v1.10 - July 20th, 2015
    o Remade Spoiler bbcode button with different font.

--- Subs-BBCode-Spoiler.php
+++ Subs-BBCode-Spoiler.php
@@ -2,6 +2,9 @@
 /**********************************************************************************
 * Subs-BBCode-Spoiler.php
 ***********************************************************************************
+* This mod is licensed under the 2-clause BSD License, which can be found here:
+*	http://opensource.org/licenses/BSD-2-Clause
+***********************************************************************************
 * This program is distributed in the hope that it is and will be useful, but      *
 * WITHOUT ANY WARRANTIES; without even any implied warranty of MERCHANTABILITY    *
 * or FITNESS FOR A PARTICULAR PURPOSE.                                            *
