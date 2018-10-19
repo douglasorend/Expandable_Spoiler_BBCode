362472c6300773626d89549b2f47ce2001742f85
douglasorend <37510276+douglasorend@users.noreply.github.com>
2018-10-18 19:10:06 -0500

    v3.2 - February 10th, 2017
    v3.2 - February 10th, 2017
    o Added code so that the border underneath the "Spoiler" text could be removed.
    o Fixed code where guests are not allowed to view spoilers, but could anyways.
    o Fixed code where guests were allowed to view spoilers, but couldn't.

--- Subs-BBCode-Spoiler.php
+++ Subs-BBCode-Spoiler.php
@@ -14,7 +14,7 @@ if (!defined('SMF'))
 
 function BBCode_Spoiler(&$bbc)
 {
-	global $txt;
+	global $txt, $modSettings, $user_info;
 
 	// Define these tags, which have no issue with block-level bbcode tags:
 	$bbc[] = array(
@@ -48,8 +48,8 @@ function BBCode_Spoiler(&$bbc)
 	$bbc[] = array(
 		'tag' => 'spoiler',
 		'type' => 'parsed_equals',
-		'before' => '<div style="padding: 3px; font-size: 1em;"><div style="text-transform: uppercase; border-bottom: 1px solid #5873B0; margin-bottom: 3px; font-size: 0.8em; font-weight: bold; display: block;"><span onClick="if (this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display != \'\') {  this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display = \'\'; this.innerHTML = \'<b>' . $txt['spoiler'] . ': $1 &#149; </b><a href=\\\'#\\\' onClick=\\\'return false;\\\'>' . $txt["debug_hide"] . '</a>\'; } else { this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display = \'none\'; this.innerHTML = \'<b>' . $txt['spoiler'] . ': $1 &#149; </b><a href=\\\'#\\\' onClick=\\\'return false;\\\'>' . $txt["debug_show"] . '</a>\'; }" /><b>' . $txt['spoiler'] . ': $1 &#149; </b><a href="#" onClick="return false;">' . $txt["debug_show"] . '</a></span></div><div class="quotecontent"><div style="display: none;">',
-		'after' => '</div></div></div>',
+		'before' => !empty($modSettings['spoiler_no_guests']) && !empty($user_info['is_guest']) ? $txt['spoiler_no_guest_html'] : '<div style="padding: 3px; font-size: 1em;"><div style="text-transform: uppercase; ' . (empty($modSettings['spoiler_no_border']) ? 'border-bottom: 1px solid #5873B0; ' : '') . 'margin-bottom: 3px; font-size: 0.8em; font-weight: bold; display: block;"><span onClick="if (this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display != \'\') {  this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display = \'\'; this.innerHTML = \'<b>' . $txt['spoiler'] . ': $1 &#149; </b><a href=\\\'#\\\' onClick=\\\'return false;\\\'>' . $txt["debug_hide"] . '</a>\'; } else { this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display = \'none\'; this.innerHTML = \'<b>' . $txt['spoiler'] . ': $1 &#149; </b><a href=\\\'#\\\' onClick=\\\'return false;\\\'>' . $txt["debug_show"] . '</a>\'; }" /><b>' . $txt['spoiler'] . ': $1 &#149; </b><a href="#" onClick="return false;">' . $txt["debug_show"] . '</a></span></div><div class="quotecontent"><div style="display: none;">',
+		'after' => !empty($modSettings['spoiler_no_guests']) && !empty($user_info['is_guest']) ?  '' : '</div></div></div>',
 		'disallow_children' => array_unique($disallowed),
 		'block-level' => true,
 	);
@@ -62,14 +62,17 @@ function BBCode_Spoiler_Validate(&$tag, &$data, &$disabled)
 	if (empty($data))
 		return ($tag['content'] = '');
 	list($text, $quote, $show, $hide, $guests, $expand) = explode('|', $tag['content']);
-	if ((!empty($modSettings['spoiler_no_guests']) || $guests == 'n' || $guests == 'no' || $guests == 'false') && !empty($user_info['is_guest']))
-		return ($tag['content'] = $txt['spoiler_no_guest_html']);
+	if (!empty($modSettings['spoiler_no_guests']) && !empty($user_info['is_guest']))
+	{
+		if (empty($guests) || $guests == 'n' || $guests == 'no' || $guests == 'false')
+			return ($tag['content'] = $txt['spoiler_no_guest_html']);
+	}
 	$text = empty($text) ? $txt['spoiler'] : $text;
 	$show = empty($show) ? $txt['debug_show'] : $show;
 	$hide = empty($hide) ? $txt['debug_hide'] : $hide;
 	$expand = ($expand == 'y' || $expand == 'yes' || $expand == 'true');
 	$data = parse_bbc($data);
-	$tag['content'] = '<div style="padding: 3px; font-size: 1em;"><div style="text-transform: uppercase; border-bottom: 1px solid #5873B0; margin-bottom: 3px; font-size: 0.8em; font-weight: bold; display: block;"><span onClick="if (this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display != \'\') {  this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display = \'\'; this.innerHTML = \'<b>' . $text . $quote . ': </b><a href=\\\'#\\\' onClick=\\\'return false;\\\'>' . $hide . '</a>\'; } else { this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display = \'none\'; this.innerHTML = \'<b>' . $text . $quote . ': </b><a href=\\\'#\\\' onClick=\\\'return false;\\\'>' . $show . '</a>\'; }" /><b>' . $text . $quote . ': </b><a href="#" onClick="return false;">' . ($expand ? $hide : $show) . '</a></span></div><div class="quotecontent"><div style="display: ' . ($expand ? '' : 'none') . ';">$1</div></div></div>';
+	$tag['content'] = '<div style="padding: 3px; font-size: 1em;"><div style="text-transform: uppercase; ' . (empty($modSettings['spoiler_no_border']) ? 'border-bottom: 1px solid #5873B0; ' : '') . 'margin-bottom: 3px; font-size: 0.8em; font-weight: bold; display: block;"><span onClick="if (this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display != \'\') {  this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display = \'\'; this.innerHTML = \'<b>' . $text . $quote . ': </b><a href=\\\'#\\\' onClick=\\\'return false;\\\'>' . $hide . '</a>\'; } else { this.parentNode.parentNode.getElementsByTagName(\'div\')[1].getElementsByTagName(\'div\')[0].style.display = \'none\'; this.innerHTML = \'<b>' . $text . $quote . ': </b><a href=\\\'#\\\' onClick=\\\'return false;\\\'>' . $show . '</a>\'; }" /><b>' . $text . $quote . ': </b><a href="#" onClick="return false;">' . ($expand ? $hide : $show) . '</a></span></div><div class="quotecontent"><div style="display: ' . ($expand ? '' : 'none') . ';">$1</div></div></div>';
 }
 
 function BBCode_Spoiler_Button(&$buttons)
@@ -88,6 +91,7 @@ function BBCode_Spoiler_Button(&$buttons)
 function BBCode_Spoiler_Settings(&$config_vars)
 {
 	$config_vars[] = array('check', 'spoiler_no_guests');
+	$config_vars[] = array('check', 'spoiler_no_border');
 }
 
 ?>
